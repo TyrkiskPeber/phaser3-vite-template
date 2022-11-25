af87835b8bde0611b8854492ec71e6091d4b28ff
TyrkiskPeber <leo.gabrielsson@elev.ga.ntig.se>
2022-11-25 11:16:20 +0100

    Started Project

---
+++ planering.md
@@ -0,0 +1,29 @@
+# Planering Leo
+## v47
+### Tis
+Brainstorm, välj mellan 2d fighter och rogue och tutorial tittande
+
+### Fre
+Kolla på tutorials och nosa på kod
+
+## v48
+### Tis
+Start the forge
+### Fre
+Forge on
+
+## v49
+### Tis
+Keep on forging
+### Fre
+Forge till we break
+
+## v50
+Skriva pm? Finslipa/provspela
+
+## v51
+### Tis
+Uppvisning
+
+### Fre
+Ledig, dag före dopparedan
--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './HelloWorldScene'
+import GameScene from './GameScene'
 
 const config = {
 	type: Phaser.AUTO,
-	parent: 'app',
 	width: 800,
 	height: 600,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 },
-		},
+			gravity: { y: 0 }
+		}
 	},
-	scene: [HelloWorldScene],
+	scene: [GameScene]
 }
 
 export default new Phaser.Game(config)
